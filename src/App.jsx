11ab52857ae69a0d7bc119249fb3deb2f2cce651
scribblenovice <pasomba41@gmail.com>
2024-01-17 13:49:15 +0100

    meta script 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,29 @@ import LaunchPad from './launchpad/Launchpad'
 import Checkout from './checkout-page/Checkout';
 
 function App() {
+ useLayoutEffect(() => {
+   function addMetaCode() {
+     var metaNoscript = document.createElement("noscript");
+     var metaScript = document.createElement("script");
+     var metaImg = document.createElement("img");
+     metaImg.setAttribute(
+       "src",
+       "https://www.facebook.com/tr?id=219032204013009&ev=PageView&noscript=1"
+     );
+     metaImg.setAttribute("width", "1");
+     metaImg.setAttribute("height", "1");
+     metaImg.style.display = "none";
+     metaNoscript.appendChild(metaImg);
+     metaScript.text =
+       "!(function(f,b,e,v,n,t,s)\r\n{if(f.fbq)return;n=f.fbq=function(){n.callMethod?\r\nn.callMethod.apply(n,arguments):n.queue.push(arguments)};\r\nif(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\r\nn.queue=[];t=b.createElement(e);t.async=!0;\r\nt.src=v;s=b.getElementsByTagName(e)[0];\r\ns.parentNode.insertBefore(t,s)})(window, document,'script',\r\n'https://connect.facebook.net/en_US/fbevents.js');\r\nfbq('init', '219032204013009');\r\nfbq('track', 'PageView');";
+     document.head.appendChild(metaScript);
+     document.head.appendChild(metaNoscript);
+     console.log("hello");
+   }
+   addMetaCode();
+ }, []);
+
+
   return (
     <BrowserRouter>
       <Routes>
