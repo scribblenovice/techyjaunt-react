e79b16c585eafff2c48d0d13e0560782efea2443
scribblenovice <pasomba41@gmail.com>
2024-01-17 22:30:59 +0100

    react facebook pixel

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,50 @@
-
-import './App.css'
-import Homepage from './homepage/Homepage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LaunchPad from './launchpad/Launchpad'
-import Checkout from './checkout-page/Checkout';
-import { useLayoutEffect } from 'react';
-
+import "./App.css";
+import Homepage from "./homepage/Homepage";
+import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
+import LaunchPad from "./launchpad/Launchpad";
+import Checkout from "./checkout-page/Checkout";
+import { useLayoutEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
 function App() {
- useLayoutEffect(() => {
-   function addMetaCode() {
-     var metaNoscript = document.createElement("noscript");
-     var metaScript = document.createElement("script");
-     var metaImg = document.createElement("img");
-     metaImg.setAttribute(
-       "src",
-       "https://www.facebook.com/tr?id=219032204013009&ev=PageView&noscript=1"
-     );
-     metaImg.setAttribute("width", "1");
-     metaImg.setAttribute("height", "1");
-     metaImg.style.display = "none";
-     metaNoscript.appendChild(metaImg);
-     metaScript.text =
-       "!(function(f,b,e,v,n,t,s)\r\n{if(f.fbq)return;n=f.fbq=function(){n.callMethod?\r\nn.callMethod.apply(n,arguments):n.queue.push(arguments)};\r\nif(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\r\nn.queue=[];t=b.createElement(e);t.async=!0;\r\nt.src=v;s=b.getElementsByTagName(e)[0];\r\ns.parentNode.insertBefore(t,s)})(window, document,'script',\r\n'https://connect.facebook.net/en_US/fbevents.js');\r\nfbq('init', '219032204013009');\r\nfbq('track', 'PageView');";
-     document.head.appendChild(metaScript);
-     document.head.appendChild(metaNoscript);
-   }
-   addMetaCode();
- }, []);
+  useLayoutEffect(() => {
+    const options = {
+      autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+      debug: false, // enable logs
+    };
+    ReactPixel.init("219032204013009", options);
+    ReactPixel.pageView(); // For tracking page view
+  });
 
+  //  useLayoutEffect(() => {
+  //    function addMetaCode() {
+  //      var metaNoscript = document.createElement("noscript");
+  //      var metaScript = document.createElement("script");
+  //      var metaImg = document.createElement("img");
+  //      metaImg.setAttribute(
+  //        "src",
+  //        "https://www.facebook.com/tr?id=219032204013009&ev=PageView&noscript=1"
+  //      );
+  //      metaImg.setAttribute("width", "1");
+  //      metaImg.setAttribute("height", "1");
+  //      metaImg.style.display = "none";
+  //      metaNoscript.appendChild(metaImg);
+  //      metaScript.text =
+  //        "!(function(f,b,e,v,n,t,s)\r\n{if(f.fbq)return;n=f.fbq=function(){n.callMethod?\r\nn.callMethod.apply(n,arguments):n.queue.push(arguments)};\r\nif(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\r\nn.queue=[];t=b.createElement(e);t.async=!0;\r\nt.src=v;s=b.getElementsByTagName(e)[0];\r\ns.parentNode.insertBefore(t,s)})(window, document,'script',\r\n'https://connect.facebook.net/en_US/fbevents.js');\r\nfbq('init', '219032204013009');\r\nfbq('track', 'PageView');";
+  //      document.head.appendChild(metaScript);
+  //      document.head.appendChild(metaNoscript);
+  //    }
+  //    addMetaCode();
+  //  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" index={true} element={<Homepage />}></Route>
         <Route path="/launchpad" element={<LaunchPad />}></Route>
-        <Route path='/checkout' element={<Checkout />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
