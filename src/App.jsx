4bf9e014165317e9827a1cfbb8aa1041757227c8
scribblenovice <pasomba41@gmail.com>
2024-01-18 17:23:16 +0100

    pixel script change

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ import Homepage from "./homepage/Homepage";
 import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import LaunchPad from "./launchpad/Launchpad";
 import Checkout from "./checkout-page/Checkout";
-// import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 // import ReactPixel from "react-facebook-pixel";
 import ThankYou from "./thank-you/ThankYou";
 import ProtectedRoute from "./globalcomponents/ProtectedRoutes";
+import { landingPageEvent } from "./globalcomponents/SitePixel";
 function App() {
-  // useLayoutEffect(() => {
-  //   const options = {
-  //     autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
-  //     debug: false, // enable logs
-  //   };
-  //   ReactPixel.init("219032204013009", options);
-  //   ReactPixel.pageView(); // For tracking page view
-  // });
+  useEffect(() => {
+    landingPageEvent();
+  });
 
   return (
     <BrowserRouter>
@@ -23,9 +19,14 @@ function App() {
         <Route path="/" index={true} element={<Homepage />}></Route>
         <Route path="/launchpad" element={<LaunchPad />}></Route>
         <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/thank-you" element={<ProtectedRoute>
-            <ThankYou />
-          </ProtectedRoute>}></Route>
+        <Route
+          path="/thank-you"
+          element={
+            <ProtectedRoute>
+              <ThankYou />
+            </ProtectedRoute>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
