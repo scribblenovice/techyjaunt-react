6a4419832d23ac00fddceb032d7a28312794848b
scribblenovice <pasomba41@gmail.com>
2024-07-02 13:09:10 +0100

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
+import 'remixicon/fonts/remixicon.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
