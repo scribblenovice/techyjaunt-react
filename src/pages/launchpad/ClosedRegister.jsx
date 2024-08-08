e93cf8a119e3b472f056ee967d339ee9187c4ec4
Asomba Obumneme <103396097+scribblenovice@users.noreply.github.com>
2024-08-08 09:51:25 +0100

    Update ClosedRegister.jsx

--- src/pages/launchpad/ClosedRegister.jsx
+++ src/pages/launchpad/ClosedRegister.jsx
@@ -4,8 +4,11 @@ const ClosedRegister = () => {
   return (
     <div className="flex justify-center items-center flex-col w-screen h-screen">
       <h1 className="text-black text-center shadow-sm font-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl tracking-widest">
-        REGISTRATION FOR COHORT 3 IS CURRENTLY CLOSED
+        REGISTRATION FOR COHORT 4 IS CURRENTLY CLOSED
       </h1>
+      <p className="mt-5"> Thanks for applying, but Cohort 4 applications are now closed.
+We have, however, added you to the waitlist for our next cohort
+Join our WhatsApp community for more updates  </p>
       <p className="my-5">
         Please go back to{" "}
         <Link className="text-blue-500" to="/">
